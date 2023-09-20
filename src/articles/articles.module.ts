b7c588749baea68cbf799021236e27c2e97a615b
mathias-kinninkpo <kinninkpomathias@gmail.com>
2023-09-21 00:30:58 +0100

    ajouts des autres fichiers

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
 import { MongooseModule,  } from '@nestjs/mongoose';
-import mongoose from 'mongoose';
 import { Article, ArticleSchema } from 'src/schema/articles.schema';
 
 
